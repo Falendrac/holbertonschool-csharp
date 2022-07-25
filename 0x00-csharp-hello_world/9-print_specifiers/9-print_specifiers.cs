202feb8cfac1c68c44f05891e79c357f64987012
Falendrac <leger_franck@hotmail.fr>
2022-07-25 12:14:11 +0200

    Add task 9.

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        var myFormat = new System.Globalization.CultureInfo("en-US");
+		Console.WriteLine(string.Format(myFormat,"Percent: {0:0.00%}\nCurrency: {1:C}", percent, currency));
+	}
+}
