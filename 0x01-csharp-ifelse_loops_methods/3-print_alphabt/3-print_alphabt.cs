1164b042bb78daf7ce89b20425f9a93a1d5f919c
Falendrac <leger_franck@hotmail.fr>
2022-07-26 11:07:17 +0200

    Add task 3 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char loop = 'a'; loop <= 'z'; loop++)
+        {
+            if (loop != 'q' && loop != 'e')
+            {
+                Console.Write(loop);
+            }
+        }
+    }
+}
