3b275a216fb8702fb3cc4f42935afa2842b6055c
Falendrac <leger_franck@hotmail.fr>
2022-07-26 11:14:29 +0200

    Add task 4 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int loop = 0; loop < 99; loop++)
+        {
+            Console.WriteLine("{0} = 0x{0:x}", loop);
+        }
+    }
+}
