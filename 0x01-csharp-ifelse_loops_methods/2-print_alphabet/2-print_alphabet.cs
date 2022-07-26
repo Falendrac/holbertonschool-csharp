436914b068c354b6c574200dcf06c9256a3a9098
Falendrac <leger_franck@hotmail.fr>
2022-07-26 10:57:33 +0200

    Add task 2 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char loop = 'a'; loop <= 'z'; loop++)
+        {
+            Console.Write(loop);
+        }
+    }
+}
