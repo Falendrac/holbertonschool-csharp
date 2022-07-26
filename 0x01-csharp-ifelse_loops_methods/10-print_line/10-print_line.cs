d1340f6154dfd3cce67fbfc0c6f6c24cd245e262
Falendrac <leger_franck@hotmail.fr>
2022-07-26 12:43:35 +0200

    Add task 10 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+        {
+            for (int loop = 0; loop < length; loop++)
+            {
+                Console.Write("_");
+            }
+        }
+        Console.WriteLine();
+    }
+}
