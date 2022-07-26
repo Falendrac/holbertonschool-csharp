5d4b386cb0e83994efd0f4687f39bfe1facfba78
Falendrac <leger_franck@hotmail.fr>
2022-07-26 12:14:14 +0200

    Add task 6 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int loop1 = 0; loop1 < 10; loop1++)
+        {
+            for (int loop2 = loop1 + 1; loop2 < 10; loop2++)
+            {
+                if (loop1 != 8)
+                {
+                    Console.Write("{0}{1}, ", loop1, loop2);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}", loop1, loop2);
+                }
+            }
+        }
+    }
+}
