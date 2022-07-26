acd0cf37ff435674a87418cab093df9d9c98c8a3
Falendrac <leger_franck@hotmail.fr>
2022-07-26 12:48:58 +0200

    Add task 11 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            for (int loop1 = 0; loop1 < length; loop1++)
+            {
+                for (int loop2 = 0; loop2 < loop1; loop2++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+        }
+        Console.WriteLine();
+    }
+}
