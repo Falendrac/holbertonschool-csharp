fa476a669a125b76ce20fa0c9d4b1de12cb47028
Falendrac <leger_franck@hotmail.fr>
2022-07-26 12:55:04 +0200

    Add task 12 to 0x01.

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int loop = 1; loop <= 100; loop++)
+        {
+            if (loop % 3 == 0 && loop % 5 == 0)
+            {
+                Console.Write("FizzBuzz");
+            }
+            else if (loop % 3 == 0)
+            {
+                Console.Write("Fizz");
+            }
+            else if (loop % 5 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            else
+            {
+                Console.Write(loop);
+            }
+
+            if (loop < 100)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
