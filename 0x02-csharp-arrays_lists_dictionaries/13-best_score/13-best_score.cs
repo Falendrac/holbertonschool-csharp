945c1454960e86c476f1a2512527d6b034d45535
Falendrac <leger_franck@hotmail.fr>
2022-07-27 14:14:20 +0200

    Add task 13 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    // returns the key with the biggest integer value in a given dictionary.
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null)
+        {
+            return "None";
+        }
+
+        var sortedDict = from entry in myList orderby entry.Value descending select entry;
+
+        return sortedDict.First().Key;
+    }
+}
