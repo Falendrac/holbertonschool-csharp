3dddd53db2a9a6d3a0a1bfc70944629697e5498a
Falendrac <leger_franck@hotmail.fr>
2022-07-27 14:27:50 +0200

    Add task 14 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] grid = new int[5, 5];
+
+        grid[2, 2] = 1;
+
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                Console.Write(grid [x, y]);
+                if (y != 4)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
