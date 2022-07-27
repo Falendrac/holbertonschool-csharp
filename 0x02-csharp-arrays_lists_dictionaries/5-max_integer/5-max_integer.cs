89c875b0840df4d7217fa36b0ad06cace14f7d7f
Falendrac <leger_franck@hotmail.fr>
2022-07-27 12:05:41 +0200

    Add task 5 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // Finds the biggest integer in a list
+    // @myList: The list to find the biggest integer
+    // Return: -1 if the list is empty, otherwise the biggest integer
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int maxInt = myList[0];
+
+        foreach (var element in myList)
+        {
+            if (maxInt < element)
+            {
+                maxInt = element;
+            }
+        }
+
+        return maxInt;
+    }
+}
