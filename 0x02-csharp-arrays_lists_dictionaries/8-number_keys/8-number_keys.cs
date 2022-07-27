4d9d03d352f0acef327104345a1109c767fef496
Falendrac <leger_franck@hotmail.fr>
2022-07-27 12:34:18 +0200

    Add task 8 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    // returns the number of keys in a dictionary
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+
+        if (myDict == null)
+        {
+            return count;
+        }
+        
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
