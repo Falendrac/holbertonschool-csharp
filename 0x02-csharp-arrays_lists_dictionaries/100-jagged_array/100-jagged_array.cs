64e5f85bc029e55313542b86dee91920d00b7e94
Falendrac <leger_franck@hotmail.fr>
2022-07-27 14:47:07 +0200

    Add task 100 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    // creates and prints a jagged array containing 3 arrays.
+    static void Main(string[] args)
+    {
+        int[][] array = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0, 1}
+        };
+
+        for (int x = 0; x < array.Length; x++)
+        {
+            for (int y = 0; y < array[x].Length; y++)
+            {
+                Console.Write(array[x][y]);
+                if (y != array[x].Length - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
