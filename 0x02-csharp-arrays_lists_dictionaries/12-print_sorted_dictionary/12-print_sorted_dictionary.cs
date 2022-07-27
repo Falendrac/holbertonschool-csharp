2a52b440d83696a4be13fd7f9e5cde9e702c89f1
Falendrac <leger_franck@hotmail.fr>
2022-07-27 13:03:09 +0200

    Add task 12 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    // prints a dictionary by ordered keys.
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sorted = myDict.OrderBy(x => x.Key);
+
+        foreach (KeyValuePair<string, string> pair in sorted)
+            Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+    }
+}
