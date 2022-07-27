4b94924d6cbf905c1944c71b4ab34e8fce94591c
Falendrac <leger_franck@hotmail.fr>
2022-07-27 12:29:37 +0200

    Add task 7 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // deletes the item at a specific position in a list.
+    // @myList: List of int where to remove an item
+    // @index: The index of element to delete
+    // Return: myList
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index > myList.Count - 1)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        int item = myList[index];
+
+        myList.Remove(item);
+
+        return myList;
+    }
+}
