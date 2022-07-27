16b874de57deba658f13d0ab358c1776f2eaf09a
Falendrac <leger_franck@hotmail.fr>
2022-07-27 12:24:13 +0200

    Add task 6 to 0x02.

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // finds all multiples of 2 in a list
+    // @myList: List of integer
+    // Return: Null if myList is null, otherwise return list of bool
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+        {
+            return null;
+        }
+
+        var result = new List<bool>();
+
+        foreach (var element in myList)
+        {
+            result.Add(element % 2 == 0);
+        }
+
+        return result;
+    }
+}
