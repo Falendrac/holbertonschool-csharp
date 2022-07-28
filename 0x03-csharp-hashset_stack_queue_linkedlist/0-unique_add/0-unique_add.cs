d65820ae80573d3a1c3820457531fc3bb03b1bd5
Falendrac <leger_franck@hotmail.fr>
2022-07-28 11:15:20 +0200

    Add task 0 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // adds all unique integers in a list.
+    public static int Sum(List<int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+        {
+            return 0;
+        }
+
+        return myList.Distinct().Sum(x => Convert.ToInt32(x));
+    }
+}
