4c8fe707a257108524bfea8cdedbe3f8efd10919
Falendrac <leger_franck@hotmail.fr>
2022-07-28 11:46:00 +0200

    Add task 2 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        int maxLength = 0;
+
+        if (list1.Count > list2.Count)
+            maxLength = list1.Count;
+        else
+            maxLength = list2.Count;
+
+        List<int> newSortedList = new List<int>();
+
+        for (int loop = 0; loop < maxLength; loop++)
+        {
+            if (loop < list1.Count)
+            {
+                if (!list2.Contains(list1[loop]))
+                    newSortedList.Add(list1[loop]);
+            }
+
+            if (loop < list2.Count)
+            {
+                if (!list1.Contains(list2[loop]))
+                    newSortedList.Add(list2[loop]);
+            }
+        }
+        
+        newSortedList.Sort();
+
+        return newSortedList;
+    }
+}
