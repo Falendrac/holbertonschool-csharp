37593cc7aa455f0953b57d6f2653d30386c6b542
Falendrac <leger_franck@hotmail.fr>
2022-07-28 15:42:59 +0200

    Add task 10 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList == null || myLList.Count == 0)
+        {
+            return 0;
+        }
+
+        int index = 0;
+
+        foreach (int element in myLList)
+        {
+            if (index == n)
+            {
+                return element;
+            }
+            
+            index++;
+        }
+
+        return 0;
+    }
+}
