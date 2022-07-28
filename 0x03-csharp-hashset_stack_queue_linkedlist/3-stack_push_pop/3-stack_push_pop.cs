55522ed17f264103923d981afda97a4501808b24
Falendrac <leger_franck@hotmail.fr>
2022-07-28 13:14:27 +0200

    Fix task 3, 3rd sentence not in if pls.

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,41 +5,26 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        string topItem = "";
-        bool findSearch = false, firstItem = false;
-        int count = 0, index = 0;
+        int countItems = aStack.Count;
 
-        foreach (string str in aStack)
-        {
-            count += 1;
-            if (firstItem == false)
-            {
-                topItem = str;
-                firstItem = true;
-            }
+        Console.WriteLine("Number of items: {0}", countItems);
 
-            if (str == search)
-            {
-                findSearch = true;
-                index = count;
-            }
-        }
-
-        if (findSearch == true)
-        {
-            for (int i = 0; i < index; i++)
-                aStack.Pop();
-        }
-        aStack.Push(newItem);
-        Console.WriteLine("Number of items: {0}", count);
-
-        if (count == 0)
+        if (countItems == 0)
             Console.WriteLine("Stack is empty");
         else
-            Console.WriteLine("Top item: {0}", topItem);
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        
+        Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
 
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, findSearch);
+        if (aStack.Contains(search))
+        {
+            while (aStack.Peek() != search)
+                aStack.Pop();
+            aStack.Pop();
+        }
 
-        return (aStack);
+        aStack.Push(newItem);
+
+        return aStack;
     }
 }
