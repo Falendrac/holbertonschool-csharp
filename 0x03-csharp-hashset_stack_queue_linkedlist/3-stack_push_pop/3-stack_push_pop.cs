225c947f0b07bdc393da103f08f7856f669dda53
Falendrac <leger_franck@hotmail.fr>
2022-07-28 13:31:37 +0200

    Rewrite all task 3.

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,27 +5,37 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        int countItems = 0;
+        int countItems = 0, index = 0;
+        string topItem = aStack.Peek();
+        bool findSearch = false;
+
+        foreach (string element in aStack)
+        {
+            countItems++;
+
+            if (element == search)
+            {
+                findSearch = true;
+                index = countItems;
+            }
+        }
+
+        if (findSearch)
+        {
+            for (int loop = 0; loop < index; loop++)
+                aStack.Pop();
+        }
+
+        aStack.Push(newItem);
 
         Console.WriteLine("Number of items: {0}", countItems);
 
         if (countItems == 0)
             Console.WriteLine("Stack is empty");
         else
-            Console.WriteLine("Top item: {0}", "BITE");
+            Console.WriteLine("Top item: {0}", topItem);
 
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, true);
-
-        if (true)
-        {
-            while ("BITE" != search)
-            {
-                aStack.Pop();
-            }
-            aStack.Pop();
-        }
-
-        aStack.Push(newItem);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, findSearch);
 
         return aStack;
     }
