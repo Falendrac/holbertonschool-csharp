aeef5f8f11816e5e89dd1c31cb2b5ebe4912e567
Falendrac <leger_franck@hotmail.fr>
2022-07-28 13:03:00 +0200

    Fix 3 unused count function.

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,7 +5,7 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        int countItems = aStack.Count;
+        int countItems = 0;
 
         Console.WriteLine("Number of items: {0}", countItems);
 
