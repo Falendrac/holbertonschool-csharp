89ee5d733acd44f4988d248a1b8cbbf160e46443
Falendrac <leger_franck@hotmail.fr>
2022-07-28 11:34:37 +0200

    Add task 1 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // returns a sorted list of common elements in two lists
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        if (list1 == null || list1.Count == 0 || list2 == null || list2.Count == 0)
+        {
+            return list1;
+        }
+
+        List<int> newSortedList = new List<int>();
+
+        for (int loop = 0; loop < list1.Count; loop++)
+        {
+            if (list2.Contains(list1[loop]))
+            {
+                newSortedList.Add(list1[loop]);
+            }
+        }
+
+        return newSortedList.Sort();
+    }
+}
