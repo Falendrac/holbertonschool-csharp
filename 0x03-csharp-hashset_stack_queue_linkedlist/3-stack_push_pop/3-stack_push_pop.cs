910b1d9772139db63907fb8c6c75019175384f7f
Falendrac <leger_franck@hotmail.fr>
2022-07-28 12:16:49 +0200

    Add task 3 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int countItems = aStack.Count;
+
+        Console.WriteLine("Number of items: {0}", countItems);
+
+        if (countItems == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        if (aStack.Contains(search))
+        {
+            Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+
+            while (aStack.Peek() != search)
+                aStack.Pop();
+            aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
