044a55673ee5dd36ea0bdd2a51f57f2a1ab6d344
Falendrac <leger_franck@hotmail.fr>
2022-07-28 15:09:13 +0200

    Add task 5 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        for (int loop = 0; loop < size; loop++)
+        {
+            llist.AddLast(loop);
+        }
+
+        foreach (int element in llist)
+            Console.WriteLine(element);
+
+        return llist;
+    }
+}
