1d750c503bd9f2b2d98130d4802f39b68980240f
Falendrac <leger_franck@hotmail.fr>
2022-07-28 12:39:15 +0200

    Fix task 1, delete condition if.

--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -6,11 +6,6 @@ class List
     // returns a sorted list of common elements in two lists
     public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
-        if (list1 == null || list1.Count == 0 || list2 == null || list2.Count == 0)
-        {
-            return list1;
-        }
-
         List<int> newList = new List<int>();
 
         for (int loop = 0; loop < list1.Count; loop++)
