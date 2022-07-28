f71e9743e932c519a6035c25b9022801e8a57ec8
Falendrac <leger_franck@hotmail.fr>
2022-07-28 13:44:57 +0200

    Add task 4, with the same code of task 3.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int countItems = 0, index = 0;
+        string topItem = "";
+        bool findSearch = false, firstItem = false;
+
+        foreach (string element in aQueue)
+        {
+            countItems++;
+
+            if (element == search)
+            {
+                findSearch = true;
+                index = countItems;
+            }
+
+            if (!firstItem)
+            {
+                topItem = element;
+                firstItem = true;
+            }
+        }
+
+        if (findSearch)
+        {
+            for (int loop = 0; loop < index; loop++)
+                aQueue.Dequeue();
+        }
+
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine("Number of items: {0}", countItems);
+
+        if (countItems == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", topItem);
+
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, findSearch);
+
+        return aQueue;
+    }
+}
