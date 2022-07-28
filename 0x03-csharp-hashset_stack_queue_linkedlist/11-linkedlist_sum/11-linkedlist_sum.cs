8d2266feed9f735b705fcc4ecb2523825dba657f
Falendrac <leger_franck@hotmail.fr>
2022-07-28 15:46:46 +0200

    Add task 11 to 0x03.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int value = 0;
+
+        foreach (int element in myLList)
+            value += element;
+
+        return value;
+    }
+}
