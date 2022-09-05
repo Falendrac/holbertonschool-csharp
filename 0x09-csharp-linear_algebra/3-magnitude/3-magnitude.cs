9e4564ae616affa0133ec280e38c8feaaedceb09
Falendrac <leger_franck@hotmail.fr>
2022-09-05 11:07:14 +0200

    Fix task 3, correction in balise

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,13 +4,13 @@
 class VectorMath
 {
     /// <summary>calculates and returns the length of a given vector.</summary>
-    /// <param name="vector">The vector given we want to calculate<param>
+    /// <param name="vector">The vector given we want to calculate</param>
     /// <returns>The length of a given vector, return -1 if is not a 2D or 3D vector</returns>
     public static double Magnitude(double[] vector)
     {
         double magnitude = 0;
 
-        if (vector.Length < 2 || vector.Length > 3)
+        if (vector.Length < 2 || vector.Length > 3)
             return -1;
 
         foreach (var element in vector)
