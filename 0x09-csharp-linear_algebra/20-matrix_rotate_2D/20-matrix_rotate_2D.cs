364c0d09c5871ca38679c0e332237ec9f1907c3d
Falendrac <leger_franck@hotmail.fr>
2022-09-05 16:19:11 +0200

    Fix 20, round directly in calculation.

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -15,9 +15,9 @@ class MatrixMath
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] {{ -1 }};
 
-        double[,] newMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
-        double[,] matrixRotation = new double[2, 2] {   {Math.Cos(angle), -1 * Math.Sin(angle)},
-                                                        {Math.Sin(angle), Math.Cos(angle)}
+        double[,] newMatrix = new double[2, 2];
+        double[,] matrixRotation = new double[, ] {   {Math.Cos(angle), Math.Sin(angle)},
+                                                        {-1 * Math.Sin(angle), Math.Cos(angle)}
                                                     };
 
         for (int row = 0; row < matrix.GetLength(0); row++)
@@ -26,9 +26,8 @@ class MatrixMath
             {
                 for (int index = 0; index < matrix.GetLength(0); index++)
                 {
-                    newMatrix[row, column] += matrix[row, index] * matrixRotation[column, index];
+                    newMatrix[row, column] += Math.Round(matrix[row, index] * matrixRotation[index, column]);
                 }
-                Math.Round(newMatrix[row, column], 2);
             }
         }
 
