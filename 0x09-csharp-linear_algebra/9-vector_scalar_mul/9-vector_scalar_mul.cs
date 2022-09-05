a619b7d9721a8ff1bf2990bd0913f2b365aaea3a
Falendrac <leger_franck@hotmail.fr>
2022-09-05 11:18:18 +0200

    Add task 9.

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/// <summary>All methods for Vector</summary>
+class VectorMath
+{
+    /// <summary>multiplies a vector and a scalar and returns the resulting vector</summary>
+    /// <param name="vector">Vector to multiply with scalar</param>
+    /// <param name="scalar">The scalar for multiply vector</param>
+    /// <returns>
+    /// If any vector is not a 2D or 3D vector, return a vector containing -1
+    /// Otherwise return the new vector
+    /// </returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return new double[1] { -1 };
+
+        double[] newVector = new double[vector.Length];
+
+        for (int browse = 0; browse < vector.Length; browse++)
+            newVector[browse] = vector[browse] * scalar;
+
+        return newVector;
+    }
+}
