343f162f76b5de3656d23f36c947e5be762c020b
Falendrac <leger_franck@hotmail.fr>
2022-09-05 16:32:08 +0200

    Fix 20, lacoucaracha

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -3,7 +3,7 @@
 /// <summary>All methods for Matrix</summary>
 class MatrixMath
 {
-    /// <summary> rotates a square 2D matrix by a given angle in radians and returns the resulting matrix.</summary>
+    /// <summary>rotates a square 2D matrix by a given angle in radians and returns the resulting matrix.</summary>
     /// <param name="matrix">The matrix to rotate</param>
     /// <param name="angle">The angle of rotation</param>
     /// <returns>
@@ -12,23 +12,22 @@ class MatrixMath
     /// </returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        if (matrix.GetLength(1) > 2)
             return new double[,] {{ -1 }};
 
-        double[,] newMatrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double[,] newMatrix = new double[2, 2];
         double[,] matrixRotation = new double[2, 2] {   {Math.Cos(angle), -Math.Sin(angle)},
                                                         {Math.Sin(angle), Math.Cos(angle)}
                                                     };
 
-        for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int row = 0; row < 2; row++)
         {
-            for (int column = 0; column < matrix.GetLength(1); column++)
+            for (int column = 0; column < 2; column++)
             {
-                for (int index = 0; index < matrix.GetLength(0); index++)
+                for (int index = 0; index < 2; index++)
                 {
-                    newMatrix[row, column] += matrix[row, index] * matrixRotation[column, index];
+                    newMatrix[row, column] = Math.Round((newMatrix[row, column] + (matrix[row, index] * matrixRotation[column, index])), 2);
                 }
-                newMatrix[row, column] = Math.Round(newMatrix[row, column], 2);
             }
         }
 
