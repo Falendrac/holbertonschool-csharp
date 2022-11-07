a22f121a3b58410588735d1e87a5722b1077be44
Falendrac <leger_franck@hotmail.fr>
2022-11-07 16:24:44 +0100

    Add task 1 to 0x0A.

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/// <summary>Do you want to see my queue ?</summary>
+class Queue<T>
+{
+    /// <summary>Get and Set the head of a list</summary>
+    public Node head { get; set; }
+    /// <summary>Get and set the tail of a list</summary>
+    public Node tail { get; set; }
+    /// <summary>The count of the node in the list</summary>
+    public int count;
+
+    /// <summary>Check the type of the queue object</summary>
+    /// <returns>The type of the queue object</returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+
+    /// <summary>Represent the node of a list</summary>
+    public class Node
+    {
+        /// <summary>Get and Set the value of a node</summary>
+        public T value { get; set; }
+        /// <summary>Get and Set the next node of this node</summary>
+        public Node next { get; set; }
+
+        /// <summary>Constructor of the node</summary>
+        /// <param name="newValue">Value of the node</param>
+        public Node(T newValue)
+        {
+            this.value = newValue;
+        }
+    }
+
+    /// <summary>Creates a new Node and adds it to the end of the queue.</summary>
+    /// <param name="value">The value of the new node<param>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (this.count == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+        }
+
+        this.count += 1;
+    }
+
+    /// <summary>The current count of nodes in the list</summary>
+    /// <returns>Return a integer count of nodes in the list</returns>
+    public int Count()
+    {
+        return this.count;
+    }
+}
