9ca908143a0ee0ef539ef872272bbdf3e28d4d14
Falendrac <leger_franck@hotmail.fr>
2022-11-07 15:37:47 +0100

    Add task 0 to 0x0A.

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>Do you want to see my queue ?</summary>
+class Queue<T>
+{
+    /// <summary>Check the type of the queue object</summary>
+    /// <returns>The type of the queue object</returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+}
