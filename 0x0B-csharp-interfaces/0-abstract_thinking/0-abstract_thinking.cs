2fce46a7340632f752eac4544b4796683370b151
Falendrac <leger_franck@hotmail.fr>
2022-11-08 14:59:35 +0100

    Add task 0 and readme

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Base is a abstract class that can be define by user
+/// </summary>
+abstract class Base
+{
+    /// <summary>The name of a person</summary>
+    public string name;
+
+    /// <summary>String representation of the class</summary>
+    /// <return>{name} is a {Type of the object}</return>
+    public override string ToString()
+    {
+        return String.Format("{0} is a {1}", name, this.GetType());
+    }
+}
