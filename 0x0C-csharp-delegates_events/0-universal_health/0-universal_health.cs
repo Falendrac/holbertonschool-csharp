bcc991197338ba19b5e98cf39ea7676b815603f7
Falendrac <leger_franck@hotmail.fr>
2022-11-09 11:38:51 +0100

    Add task 0 to 0x0C.

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/// <summary>
+/// Define a Player
+/// </summary>
+class Player
+{
+    // get or set Name of the player
+    private string name { get; set; }
+    // get or set MaxHp of the player
+    private float maxHp { get; set; }
+    // get or set hp of the player
+    private float hp { get; set; }
+
+    /// <summary>
+    /// Constructor for the player
+    /// </summary>
+    /// <param name="name">Default value is "Player"</param>
+    /// <param name="maxHp">Default value is 100f</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// Print the current health of player
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
