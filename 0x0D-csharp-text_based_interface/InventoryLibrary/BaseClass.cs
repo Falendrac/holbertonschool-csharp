714718dc824243b95400653dfa5aef33c4f1f546
Falendrac <leger_franck@hotmail.fr>
2022-11-14 16:15:01 +0100

    Create tests for BaseClass and fill the content of BaseClass

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -6,8 +6,25 @@
 public class BaseClass
 {
     /// <summary>Id of the instance</summary>
+    public string id { get; set; }
 
     /// <summary>Date that instance was created</summary>
+    public DateTime date_created { get; }
 
     /// <summary>Last date that istance was updated</summary>
+    public DateTime date_updated;
+
+    /// <summary>Constructor of BaseClass</summary>
+    public BaseClass()
+    {
+        this.id = Guid.NewGuid().ToString();
+        this.date_created = DateTime.Now;
+        this.date_updated = this.date_created;
+    }
+
+    /// <summary>Update the <see cref="date_updated"/> when it was called</summary>
+    public void Update()
+    {
+        this.date_updated = DateTime.Now;
+    }
 }
--- 0x0D-csharp-text_based_interface/InventoryManagement.Tests/BaseClass.Tests.cs
+++ 0x0D-csharp-text_based_interface/InventoryManagement.Tests/BaseClass.Tests.cs
@@ -4,16 +4,81 @@ using System;
 namespace InventoryManagement.Tests
 {
 
+    /// <summary>All tests for BaseClass</summary>
     public class BaseClassTests
     {
+        /// <summary>
+        /// Test the type of all properties
+        /// </summary>
         [Test]
         public void TestTypeProperties()
         {
             BaseClass testBaseClass = new BaseClass();
 
-            Assert.IsInstanceOf(testBaseClass.id, typeof(string));
-            Assert.IsInstanceOf(testBaseClass.date_created, typeof(DateTime));
-            Assert.IsInstanceOf(testBaseClass.date_updated, typeof(DateTime));
+            Assert.True(testBaseClass.id.GetType() == typeof(string));
+            Assert.True(testBaseClass.date_created.GetType() == typeof(DateTime));
+            Assert.True(testBaseClass.date_updated.GetType() == typeof(DateTime));
+        }
+
+        /// <summary>
+        /// Test if properties is not empty
+        /// </summary>
+        [Test]
+        public void TestIsNotEmpty()
+        {
+            BaseClass testBaseClass = new BaseClass();
+
+            Assert.IsNotNull(testBaseClass.id);
+            Assert.IsNotNull(testBaseClass.date_created);
+            Assert.IsNotNull(testBaseClass.date_updated);
+        }
+
+        /// <summary>
+        /// Test if the id of two instances is unique
+        /// </summary>
+        [Test]
+        public void TestUniqueId()
+        {
+            BaseClass testBaseClass0 = new BaseClass();
+            BaseClass testBaseClass1 = new BaseClass();
+
+            Assert.AreNotEqual(testBaseClass0.id, testBaseClass1.id);
+        }
+
+        /// <summary>
+        /// Test if the dates is not the default value of Datetime
+        /// </summary>
+        [Test]
+        public void TestDateDefault()
+        {
+            BaseClass testBaseClass = new BaseClass();
+            DateTime dt = new DateTime();
+
+            Assert.AreNotEqual(testBaseClass.date_created, dt);
+            Assert.AreNotEqual(testBaseClass.date_updated, dt);
+        }
+
+        /// <summary>
+        /// Test if date_created and date_updated was equal when the instance is created
+        /// </summary>
+        [Test]
+        public void TestDatesAtCreation()
+        {
+            BaseClass testBaseClass = new BaseClass();
+
+            Assert.AreEqual(testBaseClass.date_created, testBaseClass.date_updated);
+        }
+
+        /// <summary>
+        /// Test if date_created and date_updated was not equal when the instance is updated
+        /// </summary>
+        [Test]
+        public void TestDatesAtUpdate()
+        {
+            BaseClass testBaseClass = new BaseClass();
+            testBaseClass.Update();
+
+            Assert.AreNotEqual(testBaseClass.date_created, testBaseClass.date_updated);
         }
     }
 }
